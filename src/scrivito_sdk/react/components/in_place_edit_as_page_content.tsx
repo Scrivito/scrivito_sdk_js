409adb18f3f47647b761fed7d4c1388c3869a09d
Scrivito <scrivito@users.noreply.github.com>
2025-09-22 12:53:33 +0000

    1.54.1

--- src/scrivito_sdk/react/components/in_place_edit_as_page_content.tsx
+++ src/scrivito_sdk/react/components/in_place_edit_as_page_content.tsx
@@ -4,14 +4,13 @@ import { isInPlaceEditingActive } from 'scrivito_sdk/app_support/editing_context
 import { EditAsPageContentContext } from 'scrivito_sdk/react/edit_as_page_content_context';
 
 /** @public */
-export function InPlaceEditAsPageContent({
+export const InPlaceEditAsPageContent: React.FC<React.PropsWithChildren> = ({
   children,
-}: {
-  children: React.ReactNode;
-}) {
-  return isInPlaceEditingActive() ? (
-    <EditAsPageContentContext.Provider children={children} value={true} />
+}) =>
+  isInPlaceEditingActive() ? (
+    <EditAsPageContentContext.Provider value={true}>
+      {children}
+    </EditAsPageContentContext.Provider>
   ) : (
-    children
+    <>{children}</>
   );
-}
